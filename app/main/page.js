d3bf438a2071497a2ff18a987eb224b0bc223f66
Alizarbukhari <141038313+Alizarbukhari@users.noreply.github.com>
2025-02-22 23:50:04 +0500

    main page

--- app/main/page.js
+++ app/main/page.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import MainPic from '@/components/main/main'
+import Footer2 from '@/components/Footer2'
+import Body from '@/components/body/body'
+
 
 function Main() {
+
   return (
-    <div  >
-        <MainPic/>
+      // main div
+    <div >
+      {/* header */}
+    <div><MainPic/></div>
+    <Body/>
+
+       {/* Footer */}
+       <Footer2/>
+    
     </div>
 
   )
--- components/main/main.jsx
+++ components/main/main.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function MainPic
 
-() {
+import MainHeader from '@/components/mainheader'
+
+export default function MainPic() {
   return (
     <div>
-   <div className=" hidden md:block md:w-full md:h-[950px] ">
-    <Image 
-    src="/main/HouseMain.png"
-    alt="Picture of the Kutta"
-    width={1920}
-    height={950}
-    className='w-full h-full'
-    />
-   </div>
+      <MainHeader />
+      <div className="hidden md:block md:w-full md:h-[950px]">
+        <Image 
+          src="/main/HouseMain.png"
+          alt="Picture of the Kutta"
+          width={1920}
+          height={950}
+          className="w-full h-full"
+        />
+        <p></p>
 
-   <div className='md:hidden w-full h-[700px]'>
-   <Image 
-    src="/main/wdog.png"
-    alt="Picture of the Kutta"
-    width={390}
-    height={700}
-    className='w-full h-full'
-    />
-    
-   </div>
+      </div>
+      
+
+      <div className="md:hidden w-full h-[700px]">
+        <Image 
+          src="/main/wdog.png"
+          alt="Picture of the Kutta"
+          width={390}
+          height={700}
+          className="w-full h-full"
+        />
+      </div>
     </div>
   )
 }
