d07841ff52005dec3361ce3d2196492bcdcffcf4
Alizarbukhari <141038313+Alizarbukhari@users.noreply.github.com>
2025-02-24 22:30:35 +0500

    add vactor doghome font

--- components/main/main.jsx
+++ components/main/main.jsx
@@ -1,34 +1,16 @@
 import React from 'react'
-import Image from 'next/image'
 
-
-import MainHeader from '@/components/mainheader'
 
 export default function MainPic() {
   return (
     <div>
-      <MainHeader />
-      <div className="hidden md:block md:w-full md:h-[950px]">
-        <Image 
-          src="/main/HouseMain.png"
-          alt="Picture of the Kutta"
-          width={1920}
-          height={950}
-          className="w-full h-full"
-        />
-        <p></p>
-
-      </div>
-      
-
-      <div className="md:hidden w-full h-[700px]">
-        <Image 
-          src="/main/wdog.png"
-          alt="Picture of the Kutta"
-          width={390}
-          height={700}
-          className="w-full h-full"
-        />
+      <div className="bg-[url('/main/wdog.png')] md:bg-[url('/main/HouseMain.png')] w-full h-[100vh] relative bg-cover  ">
+        
+        <div className='md:-mt-8  flex justify-center w-full  built-titling-font '>
+          <h1 className='text-[83px] md:text-[165px]'>
+          HOUSE OF MODNEYPET
+          </h1>
+        </div>
       </div>
     </div>
   )
