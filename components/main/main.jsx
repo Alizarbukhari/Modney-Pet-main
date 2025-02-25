db394ceb2c1800d5aab38f08b4934a3b46dc1b34
Alizarbukhari <141038313+Alizarbukhari@users.noreply.github.com>
2025-02-25 20:35:34 +0500

    update main

--- components/main/main.jsx
+++ components/main/main.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function MainPic() {
   return (
     <div>
-      <div className="bg-[url('/main/wdog.png')] md:bg-[url('/main/HouseMain.png')] w-full h-[100vh] relative bg-cover  ">
+      <div className="bg-[url('/main/wdog.png')] md:bg-[url('/main/HouseMain.png')] w-[100%] h-[700px] relative bg-cover  ">
         
         <div className='md:-mt-8  flex justify-center w-full  built-titling-font '>
           <h1 className='text-[83px] md:text-[165px]'>
