ad1d219d59e4410f50d542ad4afbb6e33fe0d4d4
Hamadalishah <hammadalis459@gmail.com>
2025-02-25 20:31:15 +0500

    update main page ad form setting

--- components/main/main.jsx
+++ components/main/main.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 export default function MainPic() {
   return (
     <div>
-      <div className="bg-[url('/main/wdog.png')] md:bg-[url('/main/HouseMain.png')] w-full h-[100vh] relative bg-cover  ">
+      <div className="bg-[url('/main/wdog.png')] md:bg-[url('/main/HouseMain.png')]  w-full md:h-[100vh] h-[83vh] relative bg-contain md:bg-cover  bg-no-repeat">
         
-        <div className='md:-mt-8  flex justify-center w-full  built-titling-font '>
-          <h1 className='text-[83px] md:text-[165px]'>
+        <div className=' leading-none pt-8 pl-4 md:pl-7 built-titling-font '>
+          <h1 className='text-[83px] md:text-[200px]'>
           HOUSE OF MODNEYPET
           </h1>
         </div>
