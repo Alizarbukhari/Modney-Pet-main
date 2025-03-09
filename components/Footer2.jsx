ccbac7d04809c5562fb7e9a4978d1578b928ec59
Alizarbukhari <141038313+Alizarbukhari@users.noreply.github.com>
2025-03-09 12:49:43 +0500

    Footer complete

--- components/Footer2.jsx
+++ components/Footer2.jsx
@@ -4,8 +4,8 @@ import Image from "next/image"
 
 export default function Footer2() {
   return (
-    <div className="bg-black text-white h-[373px] md:h-[451px]  w-full flex justify-center ">
-      <div className="w-[390px] h-[224px] noto-sans-kr md:w-[1282px] md:h-[4292pxpx] px-4 md:py-12 py-14  mx-auto">
+    <div className="bg-black text-white h-[373px] md:h-[451px] items-center  w-full flex justify-center ">
+      <div className="w-[390px] h-[224px] noto-sans-kr md:w-[1282px] md:h-[4292pxpx] mx-7 md:mx-36 ">
         {/* Company Name */}
         <div>
         <h2 className="text-[14px] md:text-[24px] noto-sans-kr-bold mb-4">모드니펫 MODNEYPET</h2>
@@ -25,7 +25,7 @@ export default function Footer2() {
 
           </p>
           <p className="md:flex  md:items-start   ">
-            <p1 className="md:hidden space-x-3">
+            <p1 className="md:hidden space-x-1">
             <span className="items-start md:hidden ">이메일 manduuu_@naver.com</span>
             <span className="inline md:hidden">|</span>
             <span> 통신판매번호 2022-경기부천-2782 <a href="https://modneypet.com/index.html#none" className="underline">[사업자정보확인]</a> </span>
@@ -39,7 +39,7 @@ export default function Footer2() {
 </p1>
           
           </p>
-          <p className = "md:hidden ">            <span className="top-3"> 14786 경기도 부천시 양지로 237 (옥길동) 광장프런티어벤처5차지식산업센터 930호</span>
+          <p className = "md:hidden "><span className="top-3"> 14786 경기도 부천시 양지로 237 (옥길동) 광장프런티어벤처5차지식산업센터 930호</span>
           </p>
          
        
@@ -47,7 +47,7 @@ export default function Footer2() {
         {/* Copyright */}
 
         <div className=" ">
-        <p className="text-sm text-gray-400 mb-6">Copyright(c) All right Reserved.</p>
+        <p className="text-[10px] md:text-[15px] text-gray-400 mb-6">Copyright(c) All right Reserved.</p>
         </div>
         {/* Social Icons */}
         <div className="flex flex-wrap gap-4 md:gap-8 mb-6 ">
@@ -75,7 +75,7 @@ export default function Footer2() {
         </div>
         
      {/* border */}
-     <div className="border-[white] border-spacing-4 border-b-2 mb-6 md:mb-16">
+     <div className="border-[white] border-spacing-4 border-b-2 mb-6 md:mb-10">
 
      </div>
        
