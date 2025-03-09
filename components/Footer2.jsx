944ee04a2606022972c99674804ef531e5de04c9
Hamadalishah <hammadalis459@gmail.com>
2025-03-09 12:17:55 +0500

    make the footer responsive and complete

--- components/Footer2.jsx
+++ components/Footer2.jsx
@@ -4,31 +4,42 @@ import Image from "next/image"
 
 export default function Footer2() {
   return (
-    <div className="bg-black text-white  w-full ">
-      <div className="w-[390px] h-[373px] noto-sans-kr md:w-[1282px] md:h-[4292pxpx] px-4 md:py-12 py-14 mx-auto">
+    <div className="bg-black text-white h-[373px] md:h-[451px]  w-full flex justify-center ">
+      <div className="w-[390px] h-[224px] noto-sans-kr md:w-[1282px] md:h-[4292pxpx] px-4 md:py-12 py-14  mx-auto">
         {/* Company Name */}
         <div>
         <h2 className="text-[14px] md:text-[24px] noto-sans-kr-bold mb-4">모드니펫 MODNEYPET</h2>
         </div>
         
         {/* Contact Info */}
-        <div className="space-y-2  text-[9px] md:text-[15px] text-gray-300 mb-1">
-          <p className="md:flex space-x-3 md:items-center md:gap-4">
+        <div className="space-y-2  text-[9px] md:text-[15px] items-start  text-gray-300 mb-1">
+          <p className="md:flex space-x-[15px] md:space-x-2 items-start  ">
             <span>대표 이다영</span>
             <span className="inline">|</span>
             <span>사업자번호 343-13-00886</span>
             <span className="inline">|</span>
             <span>대표전화 010-9942-8601</span>
-            <span className="inline">|</span>
-            <span>이메일 manduuu_@naver.com</span>
+            <span className="inline space-x-0 left-0">|</span>
+            <span className="items-start hidden md:block">이메일 manduuu_@naver.com</span>
             
 
           </p>
-          <p className="md:flex space-x-3 md:items-center md:gap-4">
-          <span> 통신판매번호 2022-경기부천-2782 [사업자정보확인]</span>
-            <span className="inline">|</span>
-            <span> 14786 경기도 부천시 양지로 237 (옥길동) 광장프런티어벤처5차지식산업센터 930호</span>
+          <p className="md:flex  md:items-start   ">
+            <p1 className="md:hidden space-x-3">
+            <span className="items-start md:hidden ">이메일 manduuu_@naver.com</span>
+            <span className="inline md:hidden">|</span>
+            <span> 통신판매번호 2022-경기부천-2782 <a href="https://modneypet.com/index.html#none" className="underline">[사업자정보확인]</a> </span>
 
+            </p1>
+           
+
+             <p1 className="space-x-3 hidden md:block"><span> 통신판매번호 2022-경기부천-2782 <a href="https://modneypet.com/index.html#none" className="underline">[사업자정보확인]</a> </span>
+            <span className="hidden md:inline ">|</span>
+            <span className=""> 14786 경기도 부천시 양지로 237 (옥길동) 광장프런티어벤처5차지식산업센터 930호</span>
+</p1>
+          
+          </p>
+          <p className = "md:hidden ">            <span className="top-3"> 14786 경기도 부천시 양지로 237 (옥길동) 광장프런티어벤처5차지식산업센터 930호</span>
           </p>
          
        
@@ -64,26 +75,28 @@ export default function Footer2() {
         </div>
         
      {/* border */}
-     <div className="border-[white] border-spacing-4 border-b-2 mb-6 md:mb-8">
+     <div className="border-[white] border-spacing-4 border-b-2 mb-6 md:mb-16">
 
      </div>
        
 
 
         {/* Bottom Links */}
-        <div className="flex  gap-x-4 gap-y-2 text-[10px] md:text-[14px] text-gray-300">
+        <div className="flex mb-8   gap-x-4 gap-y-2 text-[8px] md:text-[14px] text-gray-300">
+        <span className="inline border-l-2 md:border-l-[3px]  h-[12px] md:h-[16.01px] my-auto "></span>
+
           <Link href="#" className="hover:text-white">
             개인정보처리방침
           </Link>
-          <span className="hidden md:inline">|</span>
-          <Link href="#" className="hover:text-white">
+          <span className="inline border-l-2 md:border-l-[3px]  h-[12px] md:h-[16.01px] my-auto "></span>
+          <Link href="#" className="hover:text-white h-14px">
             이용약관
           </Link>
-          <span className="hidden md:inline">|</span>
+          <span className="inline border-l-2 md:border-l-[3px]  h-[12px] md:h-[16.01px] my-auto "></span>
           <Link href="#" className="hover:text-white">
             품질보증신고
           </Link>
-          <span className="hidden md:inline">|</span>
+          <span className="inline border-l-2 md:border-l-[3px]  h-[12px] md:h-[16.01px] my-auto "></span>
           <Link href="#" className="hover:text-white">
             품질보증신고확인
           </Link>
